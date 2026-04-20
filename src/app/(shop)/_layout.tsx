997bbf9ef36fdd22d4b20e6d062d16c971edad1a
Alvin Haruna <harunaalvin69@gmail.com>
2026-04-20 12:23:48 +0100

    added bottom navigation with product and categories screens

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,13 +1,30 @@
 import { Tabs } from "expo-router";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function TabsLayout() {
   return (
-    <SafeAreaView>
-      <Tabs>
+    <SafeAreaView edges={['top']} style={styles.safeArea}>
+      <Tabs screenOptions={{
+        tabBarActiveTintColor: '#1BC464',
+        tabBarInactiveTintColor: 'gray',
+        tabBarLabelStyle: { fontSize: 16 },
+        tabBarStyle: {
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            paddingTop: 10,
+        },
+        headerShown: false,
+      }}>
         <Tabs.Screen name="index" options={{ headerShown: false }} />
         <Tabs.Screen name="orders" options={{}} />
       </Tabs>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+    }
+});
